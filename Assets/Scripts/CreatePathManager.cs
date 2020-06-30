466bb2e257c53ca3c3700ce2ca4b8a3d17f767f0
RUKA <lutca1320@gmail.com>
2020-06-30 16:25:25 +0900

    Append Path Script Complete

--- Assets/Scripts/CreatePathManager.cs
+++ Assets/Scripts/CreatePathManager.cs
@@ -7,10 +7,13 @@ public class CreatePathManager : MonoBehaviour
 {
     private Camera cm;
     public SplineComputer spline;
+    public SplineComputer SplinePreFab;
     public GameObject debugobj;
+    public int snapsize = 5;
 
-    public float last_x;
-    public float last_z;
+    private float last_x;
+    private float last_z;
+    private int new_index;
 
     private Vector3 pos;
 
@@ -18,6 +21,8 @@ public class CreatePathManager : MonoBehaviour
     void Start()
     {
         cm = GetComponentInChildren<Camera>();
+
+        new_index = spline.pointCount;
     }
 
     float SnapGrid(float value, int snapsize)
@@ -32,6 +37,11 @@ public class CreatePathManager : MonoBehaviour
         }
     }
 
+    void SpawnPath()
+    {
+        spline = Instantiate(SplinePreFab, pos, Quaternion.identity);
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -43,22 +53,30 @@ public class CreatePathManager : MonoBehaviour
             if (Physics.Raycast(ray, out hitData, 10000))
             {
                 pos = hitData.point;
+
+                UnityEngine.Debug.LogWarning(hitData.collider.gameObject.ToString());
             }
 
-            debugobj.GetComponent<Transform>().position = new Vector3(SnapGrid(pos.x, 5), 0, SnapGrid(pos.z, 5));
+            debugobj.GetComponent<Transform>().position = new Vector3(SnapGrid(pos.x, snapsize), 0, SnapGrid(pos.z, snapsize));
+            SpawnPath();
 
-            if (last_x != SnapGrid(pos.x, 5) || last_z != SnapGrid(pos.z, 5))
+            if (last_x != SnapGrid(pos.x, snapsize) || last_z != SnapGrid(pos.z, snapsize))
             {
-                UnityEngine.Debug.LogWarning("Detect!");
+                UnityEngine.Debug.LogWarning(new_index.ToString() + " Point Created!");
 
-                spline.SetPointSize(6, 1);
-                spline.SetPointPosition(6, new Vector3(SnapGrid(pos.x, 5), spline.GetPointPosition(0).y, SnapGrid(pos.z, 5)));
+                spline.SetPointNormal(new_index, spline.GetPointNormal(0));
+                spline.SetPointSize(new_index, 1);
+                spline.SetPointPosition(new_index, new Vector3(SnapGrid(pos.x, snapsize), spline.GetPointPosition(0).y, SnapGrid(pos.z, snapsize)));
 
-                last_x = SnapGrid(pos.x, 5);
-                last_z = SnapGrid(pos.z, 5);
-
-                
+                last_x = SnapGrid(pos.x, snapsize);
+                last_z = SnapGrid(pos.z, snapsize);     
             }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            new_index++;
+
+            UnityEngine.Debug.LogWarning("Up!");
         }
     }
 }
