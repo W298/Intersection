32ccff315346de2a489334c848afc085f763ab80
RUKA <lutca1320@gmail.com>
2020-11-16 23:10:08 +0900

    Implement CheckWhoGoFirst, IsCarSameWay Function

--- Assets/Contents/Scripts/CarAI.cs
+++ Assets/Contents/Scripts/CarAI.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Dreamteck.Splines;
 using SensorToolkit;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 public class CarAI : MonoBehaviour
 {
@@ -16,7 +18,42 @@ public class CarAI : MonoBehaviour
     public TriggerSensor carSensor;
     public TextMesh indicator;
 
+    public List<GameObject> excludeCars = new List<GameObject>();
+    public List<GameObject> detectedCars => carSensor.DetectedObjects.Except(excludeCars).ToList();
+
+    public GameObject detected_ClosestCar
+    {
+        get
+        {
+            GameObject closestCar;
+            var distAry = new float[detectedCars.Count];
+            var counter = 0;
+            
+            foreach (var car in detectedCars)
+            {
+                var dist = Vector3.Distance(this.gameObject.transform.position, car.transform.position);
+
+                distAry[counter] = dist;
+                counter++;
+            }
+            
+            var minIndex = Array.IndexOf(distAry, distAry.Min());
+            closestCar = detectedCars[minIndex];
+
+            return closestCar;
+        }
+    }
+
     private bool isWaiting = false;
+
+    public void AddExcludeCar(GameObject car)
+    {
+        if (!excludeCars.Contains(car))
+        {
+            excludeCars.Add(car);
+            CheckCanGo();
+        }
+    }
 
     public void EnterDT(DTBuilding dt)
     {
@@ -70,34 +107,38 @@ public class CarAI : MonoBehaviour
         carSensor = GetComponentInChildren<TriggerSensor>();
         indicator = GetComponentInChildren<TextMesh>();
         
-        carSensor.OnDetected.AddListener(OnDetected);
+        // carSensor.OnDetected.AddListener(OnDetected);
         carSensor.OnLostDetection.AddListener(OnLost);
     }
 
+    /*
     void OnDetected(GameObject obj, Sensor sensor)
     {
         if (!IsCarSameWay(obj))
         {
             if (CheckWhoGoFirst(obj))
             {
-                pathFollower.Run();
+                AddExcludeCar(obj);
             }
             else
             {
-                obj.GetComponent<PathFollower>().Run();
+                obj.GetComponent<CarAI>().AddExcludeCar(this.gameObject);
             }
         }
         
         pathFollower.Stop();
     }
+    */
 
     bool IsCarSameWay(GameObject obj)
     {
-        if (pathFollower.splineFollower.spline == obj.GetComponent<PathFollower>().splineFollower.spline)
+        if (pathFollower.splineFollower.spline == obj.GetComponent<PathFollower>().splineFollower.spline &&
+            pathFollower.splineFollower.direction == obj.GetComponent<PathFollower>().splineFollower.direction)
             return true;
-        
-        var a = Vector3.Angle(gameObject.transform.forward, obj.transform.forward) <= 30;
-        return a;
+
+        var a = Vector3.Angle(gameObject.transform.forward, obj.transform.forward);
+        indicator.text = a.ToString();
+        return a <= 70;
     }
 
     bool CheckWhoGoFirst(GameObject obj)
@@ -105,33 +146,65 @@ public class CarAI : MonoBehaviour
         // True - I go
         // False - You go
 
-        var r = Random.Range(0, 2);
-        return r != 0;
+        var thisPer = pathFollower.splineFollower.spline.Project(this.gameObject.transform.position).percent;
+        var oppPer = obj.GetComponent<PathFollower>().splineFollower.spline.Project(obj.transform.position).percent;
+
+        return thisPer >= oppPer;
     }
 
     void OnLost(GameObject obj, Sensor sensor)
     {
-        if (carSensor.DetectedObjects.Count == 0 && !isWaiting)
+        CheckCanGo();
+
+        if (excludeCars.Contains(obj))
+        {
+            excludeCars.Remove(obj);
+        }
+    }
+
+    bool CheckCanGo()
+    {
+        if (detectedCars.Count == 0 && !isWaiting)
         {
             pathFollower.Run();
+            return true;
         }
+
+        return false;
     }
 
     void Update()
     {
-        // indicator.text = carStat.ToString();
-
-        if (carSensor.DetectedObjects.Count != 0)
+        if (detectedCars.Count != 0)
         {
-            if (carSensor.DetectedObjects[0] == gameObject && CheckWhoGoFirst(carSensor.DetectedObjects[0]))
+            if (!excludeCars.Contains(detected_ClosestCar))
             {
-                pathFollower.Run();
-                indicator.text = "I GO";
+                if (!IsCarSameWay(detected_ClosestCar))
+                {
+                    if (detected_ClosestCar.GetComponent<CarAI>().detected_ClosestCar == gameObject)
+                    {
+                        if (CheckWhoGoFirst(detected_ClosestCar))
+                        {
+                            AddExcludeCar(detected_ClosestCar);
+                        }
+                        else
+                        {
+                            detected_ClosestCar.GetComponent<CarAI>().AddExcludeCar(this.gameObject);
+                        }
+                    }
+                    else
+                    {
+                        pathFollower.Stop();
+                    }
+                }
+                else
+                {
+                    pathFollower.Stop();
+                }
             }
             else
             {
-                carSensor.DetectedObjects[0].GetComponent<PathFollower>().Run();
-                indicator.text = "You GO";
+                pathFollower.Stop();
             }
         }
     }
